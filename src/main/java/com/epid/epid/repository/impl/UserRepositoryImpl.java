0f56d71a1f821c04011736eb0fa441181561d7c9
GorbatenkoDmitry <dima.gorbatenko.1998@mail.ru>
2025-02-25 13:06:32 +0300

    25/02/2025

--- src/main/java/com/epid/epid/repository/impl/UserRepositoryImpl.java
+++ src/main/java/com/epid/epid/repository/impl/UserRepositoryImpl.java
@@ -8,9 +8,55 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 @Repository
 public class UserRepositoryImpl implements UserRepository {
+
+    private final String FIND_BY_ID """
+SELECT u.id as user_id,
+    u.name as user_name,
+    u.username as user_username,
+    u.password as user_password,
+    ur.role as user_role,
+    FROM users u
+    LEFT JOIN users_role ur on u.id = ur.user_id
+    WHERE u.id = ?
+    """;
+    
+   private final String FIND_BY_USERNAME """
+SELECT u.id as user_id,
+    u.name as user_name,
+    u.username as user_username,
+    u.password as user_password,
+    ur.role as user_role,
+    FROM users u
+    LEFT JOIN users_role ur on u.id = ur.user_id
+    WHERE u.username = ?
+    """;
+
+
+  private final String DELETE = """
+            DELETE FROM users
+            WHERE id = ?""";
+
+    private final String CREATE = """
+            INSERT INTO users (name,username,password)
+            VALUES (?,?,?)""";
+
+    private final String UPDATE = """
+            UPDATE users
+            SET name = ?,
+            username = ?,
+            password = ?
+            WHERE id = ?""";
+
+    private final String INSERT_USER_STATUS = """
+          INSERT INTO users_roles (users_id,role)
+            VALUES (?,?)""";
+
+
     @Override
     public Optional<User> findById(Long id) {
-        return Optional.empty();
+
+    
+    
     }
 
     @Override
