fcecde55db113eca92fc0a871090fcd9d73d97a0
GorbatenkoDmitry <dima.gorbatenko.1998@mail.ru>
2025-03-04 12:46:51 +0300

    04/03/2025

--- src/main/java/com/epid/epid/repository/impl/VaccinationsRepositoryImpl.java
+++ src/main/java/com/epid/epid/repository/impl/VaccinationsRepositoryImpl.java
@@ -10,7 +10,7 @@ SELECT v.id as Vaccinations_id,
     v.Vaccinations as Vaccinations_ADSM,
     v.Vaccinations as Vaccinations_HepatitisB,
     v.Vaccinations as Vaccinations_Measles,
-   v.Vaccinations as Vaccinations_Measles,
+   v.Vaccinations as Vaccinations_Rubella,
     FROM Vaccinations v
     LEFT JOIN users_id vi on v.id = vi.Vaccinations_id
     WHERE v.id = ?
@@ -20,8 +20,17 @@ SELECT v.id as Vaccinations_id,
             SET ADSM = ?,
             HepatitisB = ?,
             Measles = ?
+            Rubella = ?
             WHERE id = ?""";
 
+private final String CREATE = """
+            INSERT INTO Vaccinations (ADSM,HepatitisB,Measles,Rubella)
+            VALUES (?,?,?,?)""";
+
+
+  private final String DELETE = """
+            DELETE FROM Vaccinations
+            WHERE id = ?""";
 
     @Override
     public Vaccinations getById(Long id) {
