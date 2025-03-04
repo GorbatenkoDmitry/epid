73b82ceec16423e022fe93d5d69b53aaecab7bf8
GorbatenkoDmitry <dima.gorbatenko.1998@mail.ru>
2025-03-04 12:44:36 +0300

    04/03/2025

--- src/main/java/com/epid/epid/repository/impl/VaccinationsRepositoryImpl.java
+++ src/main/java/com/epid/epid/repository/impl/VaccinationsRepositoryImpl.java
@@ -4,6 +4,24 @@ import com.epid.epid.domain.vaccinations.Vaccinations;
 import com.epid.epid.repository.VaccinationsRepository;
 
 public class VaccinationsRepositoryImpl implements VaccinationsRepository {
+    
+  private final String FIND_BY_ID """
+SELECT v.id as Vaccinations_id,
+    v.Vaccinations as Vaccinations_ADSM,
+    v.Vaccinations as Vaccinations_HepatitisB,
+    v.Vaccinations as Vaccinations_Measles,
+   v.Vaccinations as Vaccinations_Measles,
+    FROM Vaccinations v
+    LEFT JOIN users_id vi on v.id = vi.Vaccinations_id
+    WHERE v.id = ?
+    """;
+   private final String UPDATE = """
+            UPDATE Vaccinations
+            SET ADSM = ?,
+            HepatitisB = ?,
+            Measles = ?
+            WHERE id = ?""";
+
 
     @Override
     public Vaccinations getById(Long id) {
