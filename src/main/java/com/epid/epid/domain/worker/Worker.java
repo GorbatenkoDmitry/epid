b7b1f219203809ea53bb5f4eb7485b0761c681a4
GorbatenkoDmitry <dima.gorbatenko.1998@mail.ru>
2025-04-09 13:05:56 +0300

    Update Worker.java

--- src/main/java/com/epid/epid/domain/worker/Worker.java
+++ src/main/java/com/epid/epid/domain/worker/Worker.java
@@ -3,11 +3,11 @@ package com.epid.epid.domain.worker;
 import com.epid.epid.domain.vaccinations.Vaccinations;
 import lombok.Getter;
 import lombok.Setter;
-
+import java.io.Serializable
 import java.util.List;
 @Getter
 @Setter
-public class Worker {
+public class Worker implements Serializable{
     private Long id;
     private String name;
     private String surname;
