2a1bd8ee1a447ce50453a698d19dcd423e009a7d
GorbatenkoDmitry <dima.gorbatenko.1998@mail.ru>
2025-05-07 06:34:40 +0300

    Update VaccinationsController.java

--- src/main/java/com/epid/epid/web/controller/VaccinationsController.java
+++ src/main/java/com/epid/epid/web/controller/VaccinationsController.java
@@ -19,16 +19,22 @@ public class VaccinationsController {
     private final VaccinationsService vaccinationsService;
     private final VaccinationsMapper vaccinationsMapper;
 
+            @Operation(summary = "Get вакцинацию by id")
     @GetMapping("/{id}")
     public VaccinationsDto getVaccinationsById(@PathVariable Long id) {
         Vaccinations vaccinations = vaccinationsService.getById(id);
         return vaccinationsMapper.toDto(vaccinations);
     }
-    @GetMapping("/{name}")
+          
+        
+@Operation(summary = "Get вакцинацию by worker id")
+    @GetMapping("/{worker_id}")
     public VaccinationsDto getVaccinationsByWorkerId(@PathVariable Long worker_id) {
         Vaccinations vaccinations = vaccinationsService.getByWorkerId(worker_id);
         return vaccinationsMapper.toDto(vaccinations);
     }
+       
+    @Operation(summary = "Create вакцинацию")
     @PostMapping("/{id}/vaccinations/create")
 
     public VaccinationsDto createVaccinations(@PathVariable Long id, @Validated(OnCreate.class) @RequestBody VaccinationsDto dto
@@ -37,6 +43,8 @@ public class VaccinationsController {
         Vaccinations createdVaccinations = vaccinationsService.create(vaccinations, id);
         return vaccinationsMapper.toDto(createdVaccinations);
     }
+
+@Operation(summary = "Delete вакцинацию")
     @DeleteMapping("/{id}")
     public void VaccinationsDeleteById(@PathVariable Long id) {
         vaccinationsService.delete(id);
