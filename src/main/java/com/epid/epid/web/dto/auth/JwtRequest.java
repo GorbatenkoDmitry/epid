8c83b9a68407dbd0df6b90805b8d6528b44fce9b
GorbatenkoDmitry <dima.gorbatenko.1998@mail.ru>
2025-05-07 06:59:36 +0300

    Update JwtRequest.java

--- src/main/java/com/epid/epid/web/dto/auth/JwtRequest.java
+++ src/main/java/com/epid/epid/web/dto/auth/JwtRequest.java
@@ -4,9 +4,14 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 //Данный класс будет проверять введенные данные с фронта с контроллера и затем будет возвращаться в JwtResponse
 @Data
+    @Schema(description = "Request for login")
+
 public class JwtRequest {
+    @Schema(description = "email",example = "johndoe@gmail.com")
     @NotNull(message = "Username must be not null.")
     private String username;
+    
+    @Schema(description = "password",example = "12345")
     @NotNull(message = "Password must be not null.")
     private String password;
 }
